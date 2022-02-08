c064b3901a61821e49f9a0e3a5eae147211c3563
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-02-08 21:28:56 +0900

    Feat : ObjectPool 기능 추가
    노트 ObjectPool를 사용하여 재사용하도록 함

---
+++ Animal_Concert/Assets/Resources/Scripts/ObjectPool.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField]
+    private int noteCount;
+
+    private void Start()
+    {
+        
+    }
+
+    public void NoteCreate()
+    {
+
+    }
+}
---
+++ Animal_Concert/Assets/Resources/Scripts/ObjectPoolManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    private static ObjectPoolManager instance;
+    public static ObjectPoolManager Instance
+    {
+        get
+        {
+            return Instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+}
