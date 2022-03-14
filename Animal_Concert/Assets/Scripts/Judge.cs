9ab75e9c75ffb1d001215450cf4df7e82211c4b0
naeun0203 <72336040+naeun0203@users.noreply.github.com>
2022-03-14 22:30:28 +0900

    Feat : Combo 텍스트 추가 ++ 동물 애니메이션 추가
    버튼 클릭시 Combo 텍스트와 판정정보 화면에 나타나는 기능 추가함

--- Animal_Concert/Assets/Scripts/Judge.cs
+++ Animal_Concert/Assets/Scripts/Judge.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Judge : MonoBehaviour
-{
+{ 
+    public Text ComboText;
+    public Text JudgeText;
     public Text ScoreText;
     public Image HP_Fill;
     private int Score = 0;
+    private int Combo = 0;
     private void ChangeScore(int score)
     {
         Score += score;
@@ -18,22 +21,40 @@ public class Judge : MonoBehaviour
         HP_Fill.fillAmount += hp;
     }
     
+    private void ChangeCombo(int combo)
+    {
+        Combo += combo;
+        ComboText.text = Combo.ToString();
+    }
+    private void ChangeJudge(string judge)
+    {
+        JudgeText.text = judge;
+    }
     public void CurrentJudge(string judge)
     {
         switch(judge)
         {
             case "Miss":
+                Combo = 0;
+                ComboText.text = null;
                 ChangeHP(-0.1f);
+                ChangeJudge("MISS");
                 break;
             case "Bad":
                 ChangeScore(1);
+                ChangeCombo(1);
+                ChangeJudge("BAD");
                 break;
             case "Great":
                 ChangeScore(5);
+                ChangeCombo(1);
+                ChangeJudge("GREAT");
                 break;
             case "Perfect":
                 ChangeScore(10);
+                ChangeCombo(1);
                 ChangeHP(0.01f);
+                ChangeJudge("PERFECT");
                 break;
         }
     }
